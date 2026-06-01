de3ad7683c395ad9ca3b869968b7cf64221e110c
ilham0209 <danialilham53@gmail.com>
2026-06-01 12:16:35 +0800

    point to api

--- Constants/AppConstants.cs
+++ Constants/AppConstants.cs
@@ -3,7 +3,8 @@ namespace DuitTracker.Frontend.Constants;
 public static class AppConstants
 {
     public const string AppName = "DuitTracker";
-    public const string ApiBaseUrl = "http://localhost:5158/";
+    // public const string ApiBaseUrl = "http://localhost:5158/";
+    public const string ApiBaseUrl = "https://duittrackerapi-production.up.railway.app/";
     public const string IncomeType = "Income";
     public const string ExpenseType = "Expense";
 }
