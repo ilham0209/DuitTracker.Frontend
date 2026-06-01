44aae1360231eb7f3ddc16bb3bb18d6f8ac1931e
ilham0209 <danialilham53@gmail.com>
2026-06-01 19:51:38 +0800

    fix submodule issue add publish to gitignore

---
+++ publish/wwwroot/_framework/blazor.webassembly.js
@@ -0,0 +1,1 @@
+document.write('<script src="_framework/blazor.webassembly.66stpp682q.js"><\/script>');
---
+++ publish/wwwroot/_framework/dotnet.native.js
@@ -0,0 +1,1 @@
+document.write('<script src="_framework/dotnet.native.mx9wzm9o5h.js"><\/script>');
